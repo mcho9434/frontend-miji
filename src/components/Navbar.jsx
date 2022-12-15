3889065e60b00dbe94705e81e43416e56a066874
mcho9434 <mcho9434@usc.edu>
2022-12-15 11:11:26 -0800

    add github link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { Outlet, Link, useLocation } from "react-router-dom";
 import EmailIcon from "@mui/icons-material/Email";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 const Navbar = () => {
   const location = useLocation();
@@ -24,6 +25,15 @@ const Navbar = () => {
               }
             >
               <LinkedInIcon className="!w-8 !h-8" />
+            </div>
+            <div
+              className="p-3 hover:bg-gray-500  border border-black rounded-lg cursor-pointer"
+              onClick={() =>
+                (window.location =
+                  "https://www.linkedin.com/in/matthew-cho-usc/")
+              }
+            >
+              <GitHubIcon className="!w-8 !h-8" />
             </div>
           </div>
           <div className="text-xl">
