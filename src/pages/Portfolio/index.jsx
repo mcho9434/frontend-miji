e13f31fe3282dba316ea4ba3f7632637913ce376
mcho9434 <mcho9434@usc.edu>
2022-12-15 11:22:58 -0800

    setup isMobile

--- src/pages/Portfolio/index.jsx
+++ src/pages/Portfolio/index.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import Card from "../../components/Card";
 import Beacons from "./Beacons";
 import Otis from "./Otis";
+import useIsMobile from "../../utils/useIsMobile";
 const Portfolio = () => {
+  const isMobile = useIsMobile();
+  console.log(isMobile);
   return (
     <Card>
+      {isMobile}
       <div className="mb-4">
         This is the portfolio page! Here's a list of stuff I've worked on or
         been a part of in the past:
