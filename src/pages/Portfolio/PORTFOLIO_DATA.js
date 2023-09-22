569c2bca20af7d3a57b0faaf66e2b589db05f206
Matthew <mcho9434@usc.edu>
2023-09-22 03:02:47 -0700

    refactor pub file structure

--- src/pages/Portfolio/PORTFOLIO_DATA.js
+++ src/pages/Portfolio/PORTFOLIO_DATA.js
@@ -4,7 +4,7 @@ const PORTFOLIO_DATA = [
     location: "San Francisco, California",
     date: "May 2023 - August 2023",
     body: "I returned to Beacons the summer of 2023. Having interned there in the past, I really wanted to have new experiences. I started off working on the email marketing product, then switched teams to the store product. Since I'd worked extensively with many members of the team, I got to work on many higher trust and risk tasks. I was also given tasks under my sole responsibility(DRI-Directly Responsible Individual). For example, I got to write the suspension system for the entire product complete with suspension audits, rule enforcement, and multiple frontend dashboards. I also added the first email notification system for the app and even contributed several new components to the Storybook library.",
-    src: "lavalab1.jpg",
+    src: "portfolio/lavalab1.jpg",
     alt: "cora",
   },
   {
@@ -12,7 +12,7 @@ const PORTFOLIO_DATA = [
     location: "San Francisco, California",
     date: "August 2022 - December 2022",
     body: "I returned to Beacons the summer of 2023. Having interned there in the past, I really wanted to have new experiences. I started off working on the email marketing product, then switched teams to the store product. Since I'd worked extensively with many members of the team, I got to work on many higher trust and risk tasks. I was also given tasks under my sole responsibility(DRI-Directly Responsible Individual). For example, I got to write the suspension system for the entire product complete with suspension audits, rule enforcement, and multiple frontend dashboards. I also added the first email notification system for the app and even contributed several new components to the Storybook library.",
-    src: "winnie.jpg",
+    src: "portfolio/winnie.jpg",
     alt: "office dog",
   },
   {
@@ -20,7 +20,7 @@ const PORTFOLIO_DATA = [
     location: "Farmington, Connecticut",
     date: "May 2022 - July 2022",
     body: "Otis is the world’s largest elevator and escalator manufacturer and servicer; I joined the compass360 team, working on interactive touchscreen terminals. I worked hard to modernize the outdated codebase, even pitching modern frameworks to engineering directors. I eventually rewrote the majority of the functionality into React, saving on payload size and load times. It was my first embedded software experience, so it was super cool to have hands-on experience! I’ve attached an image of the terminal I would interact with on a daily basis.",
-    src: "det.jpg",
+    src: "portfolio/det.jpg",
     alt: "cora",
   },
   {
@@ -28,7 +28,7 @@ const PORTFOLIO_DATA = [
     location: "Los Angeles, California",
     date: "January 2022 - Present",
     body: "Lavalab is a student-run entrepreneurial incubator program that runs over the course of a semester from ideation to demo-day. My team and I pitched Cora, an emotionally intelligent toy for children to help regulate emotions. It was great to wear so many hats in a short time from developer to pitch-deck maker. At the end of the semester my team and I pitched to a panel of venture capitalists and entrepreneurs. Here’s some pictures of Cora(nervous) who was a combination of a dollar-store stuffed animal and an Arduino Uno!",
-    src: "lavalab1.jpg",
+    src: "portfolio/lavalab1.jpg",
     alt: "cora",
   },
   {
@@ -36,7 +36,7 @@ const PORTFOLIO_DATA = [
     location: "Los Angeles, California",
     date: "August 2020 - Present",
     body: "Code the Change is a group of self-driven students engaged in the pro-bono development of products for LA-based nonprofits. I’ve been a member since I was a freshman and have taken on a plethora of roles, everything from entry-level developer to tech lead to head of product. I’ve worked with everything from non-profits focused on urban gardening to preventing human trafficking. It’s been awesome to work with like minded peers and local non-profits. Here’s a picture of my team and I in an urban garden in Los Angeles!",
-    src: "CTC.jpg",
+    src: "portfolio/CTC.jpg",
     alt: "code the change team",
   },
 ];
