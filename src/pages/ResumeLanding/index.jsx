93426ab0ba9af4beb599ae5690e0c0de1164c54d
Matthew <mcho9434@usc.edu>
2023-09-22 03:41:25 -0700

    website done for now

--- src/pages/ResumeLanding/index.jsx
+++ src/pages/ResumeLanding/index.jsx
@@ -10,19 +10,15 @@ const ResumeLanding = () => {
   return (
     <Card>
       <div className={clsx("flex m-4 gap-2", { "flex-col": isMobile })}>
-        {isMobile && (
-          <img className="w-1/2 m-auto" src="matt.jpg" alt="CTC RFP" />
-        )}
-
         <div className="mt-3">
-          <div className="mt-2 text-lg font-semibold">
+          <div className="mt-2 text-3xl font-semibold">
             &emsp;If you're on this page(rather than{" "}
             <a href="/" className="underline">
               this one
             </a>
             ), it means you came from my resume or a private link.
           </div>
-          <div className="mt-2">
+          <div className="mt-2 text-xl">
             &emsp;
             <a
               href="https://www.nngroup.com/articles/how-long-do-users-stay-on-web-pages/"
@@ -32,25 +28,23 @@ const ResumeLanding = () => {
             </a>{" "}
             have found that the average user spends less than a minute before
             leaving a website. You can read all about the work I've done on my
-            resume and in the portfolio section of this website, but I want to
-            spend your valuable seconds emphasizing my{" "}
+            resume and in the portfolio section of this website, but{" "}
             <span className="font-bold">
-              drive, communication, and ownership.
+              please spend your valuable seconds reading about my skills that
+              won’t show up on my resume.
             </span>
           </div>
-          <div className="mt-2">
-            &emsp;Even as a college student, I've already devoted thousands of
-            dev hours working with product managers, tech leads, and designers.
-            I've learned through experience how to communicate my ideas clearly,
-            but also to take ownership of my work when things go wrong. If I am
-            responsible for a page or feature, I'm the first one to address
-            potential issues and cover vulnerabilites. Furthermore, I'm the
-            engineer who has the drive to spend the extra time making things to
-            a high standard. While I consider myself a great coder, I truly
-            believe I'm an oustanding team member for my ability to express
-            myself clearly and fully take on responsibilities.
+          <div className="mt-2 text-xl">
+            &emsp;Behind the scenes, I've already devoted thousands of dev hours
+            working with product managers, tech leads, and designers. It’s
+            helped to hone my development skills, and I’m definitely proud of
+            that. But more importantly I gained a ​​commitment to effective
+            communication and learned to take ownership for my work. Beyond
+            coding knowledge, this combination has allowed me to consistently
+            ship quality products, improve my work when applicable, and be an
+            trusted, effective member of my team.
           </div>
-          <div className="mt-2">
+          <div className="mt-2 text-xl">
             &emsp;I know this is just my perspective, so I've even collected{" "}
             <a className="underline" href="recs.png" target="_blank">
               testimonials
@@ -64,13 +58,11 @@ const ResumeLanding = () => {
             >
               Linkedin
             </a>{" "}
-            from my previous managers who have observed these qualities. Please
-            reach out, I would love to chat about future opportunities.
+            from my previous PM's/TL's who have also observed these qualities in
+            me. Thanks for reading! This took a while to build, but should only
+            take you ~30 seconds to read :)
           </div>
         </div>
-        {!isMobile && (
-          <img className="w-1/2 m-auto" src="matt.jpg" alt="CTC RFP" />
-        )}
       </div>
     </Card>
   );
